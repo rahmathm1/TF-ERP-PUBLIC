9b9f9f92241033efeeaa0a23277ed0867c2782f9
Rahmathullah M <rahmathullah@nuevalgo.com>
2013-04-18 10:51:47 +0530

    live now

--- scripts/common.js
+++ scripts/common.js
@@ -3,7 +3,7 @@
 	var ALERT_TITLE	=	"Taskforces ERP";
 	//var API			= 	"http://192.168.1.188/taskforces/api/";
 	//var API			= 	"http://192.168.1.155:88/taskforces/api/";
-	var API			= 	"http://192.168.31.17/taskforces/api/";
+	var API			= 	"http://203.124.121.150:88/taskforces/api/";
 	
 	/**************************************************************
 	*			GENERAL FUNCTIONS	
